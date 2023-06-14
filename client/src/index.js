d05b63f89549d7fed7796a04958a1646fbd252fd
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2023-06-14 10:40:17 -0300

    deploy front

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,24 @@
 // Import react utilities:
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 // Import app component and store:
-import App from './App';
-import { store } from './store';
+import App from "./App";
+import { store } from "./store";
 // import reportWebVitals from './reportWebVitals';
 
 // Import style:
-import './index.css';
+import "./index.css";
 
 // TODO: deploy (down):
-import axios from 'axios';
+import axios from "axios";
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 axios.defaults.baseURL =
-  // `https://videogames-web-production.up.railway.app/` ||
-  'http://localhost:3000';
-// TODO: deploy (up)
+  `https://project-gaming-beyond-v2-production.up.railway.app/` ||
+  "http://localhost:3000";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -27,7 +26,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // reportWebVitals();
