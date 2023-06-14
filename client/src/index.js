03639b07c7ff80a31fcff4cb0f0e9e364c8c214d
Federico Irarrazaval <federico.irarrazaval@itglobers.com>
2023-06-14 16:24:49 -0300

    fix dotenv issue

--- client/src/index.js
+++ client/src/index.js
@@ -10,10 +10,10 @@ import { store } from "./store";
 // Import style:
 import "./index.css";
 
-// TODO: deploy (down):
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+// // TODO: deploy (down):
+// import axios from "axios";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 axios.defaults.baseURL =
   `https://project-gaming-beyond-v2-production.up.railway.app/` ||
